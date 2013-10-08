94b2922b4a4078e87658da178349f0ba99171c9b
R. S. Doiel <rsdoiel@gmail.com>
2013-10-08 15:12:13 -0700

    Create storage_test.js

---
+++ www/js/storage_test.js
@@ -0,0 +1,8 @@
+/**
+ * storage_test.js - test the storage.js module.
+ */
+/*jslint browser: true, indent: 4 */
+(function (global) {
+  "use strict";
+  throw "storage Tests Not implemented.";
+}(this));
